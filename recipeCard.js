f9f6e071029a16a796521f9e834a74a3e2d42e2f
jeanmax1me <jeanmaximegilbert@gmail.com>
2023-11-09 13:40:04 +0100

    layout beta

--- recipeCard.js
+++ recipeCard.js
@@ -5,7 +5,7 @@ const card = document.createElement('div');
 card.style.width = '380px';
 card.style.height = '731px';
 card.style.backgroundColor = 'pink';
-card.innerText = '<p>This is a recipe card!</p>';
+card.innerText = 'Test JS 123 456';
 card.style.border = '2px solid red';
 
 // Find the cards-container by its class
